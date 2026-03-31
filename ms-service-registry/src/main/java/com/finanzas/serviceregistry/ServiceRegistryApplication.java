5dd8265fe490c1226efff04ae569ecb471c95d60
Ivan Rene Caro <ing.ivancaro9124@gmail.com>
2026-03-31 14:12:30 -0500

    se agregan comentarios en service registry de eureka

--- ms-service-registry/src/main/java/com/finanzas/serviceregistry/ServiceRegistryApplication.java
+++ ms-service-registry/src/main/java/com/finanzas/serviceregistry/ServiceRegistryApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// Clase principal del microservicio Service Registry (Eureka Server).
+// Este servicio actúa como un directorio central donde todos los demás microservicios
+// se registran para poder encontrarse entre sí en una arquitectura de microservicios.
+// Es el primer servicio que debe iniciarse en el sistema.
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer  // Habilita el servidor Eureka para descubrimiento de servicios
 public class ServiceRegistryApplication {
+    
+    // Método principal que inicia el servidor Eureka.
+    // Este método es el punto de entrada de la aplicación Spring Boot.
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
